7735e9d08c7acb5e34a5dd40c60ec5ecdeb1ed96
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-25 10:36:24 -0400

    more tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,7 +7,7 @@ context('Chainsaw Direct', () => {
     cy.visit('/')
   })
 
-  it('has gas chainsaws', () => {
+  it('has reasonably priced gas chainsaws', () => {
     cy.get('#style1').select('Chain Saws - Gas')
     cy.contains('.plcount', /^\d\d Products$/).should('be.visible')
 
@@ -18,7 +18,36 @@ context('Chainsaw Direct', () => {
         const prices = $prices
           .toArray()
           .map($el => parseFloat($el.innerText.substr(1)))
-        console.log(prices)
+        expect(prices).to.be.sorted()
       })
+  })
+
+  it('finds Cypress among the saws', () => {
+    cy.server()
+    cy.route('/sayt.php?q=Cypr*', {
+      suggestions: [
+        {
+          value: 'Cypress Test Runner',
+          data: 1,
+          exactMatch: 1
+        }
+      ]
+    })
+
+    // by stubbing search XHRs we can return a single
+    // result when typing "Cypress" into the search box
+    cy.get('#searchText').type('Cypress')
+    cy.get('.autocomplete-suggestion')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Cypress Test Runner')
+      .click()
+
+    // after selecting the synthetic search result
+    // the returned page shows that search in the input search box
+    cy.url().should('match', /\/search-results.php/)
+    cy.get('input#searchText')
+      .should('have.value', 'Cypress Test Runner')
+      .and('be.visible')
   })
 })
